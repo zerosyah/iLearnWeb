f0830f77c59a15b23784b48d4ba7d865e8c52c9a
zerosyah <syahshezi3000@gmaail.com>
2025-09-04 20:50:22 +0200

    iLearnWeb v1.0.0 Header page update

--- src/pages/Department.tsx
+++ src/pages/Department.tsx
@@ -1,5 +1,6 @@
 
 import { Card } from "flowbite-react";
+import Header from "../components/Header";
 
 export default function Department() {
   const subjects = [
@@ -32,7 +33,7 @@ export default function Department() {
       subjects[5],
       subjects[6],
       subjects[17],
-      subjects[17]+1,
+      subjects[17],
     ],
     Grade9: [
       subjects[0],
@@ -43,7 +44,7 @@ export default function Department() {
       subjects[5],
       subjects[6],
       subjects[17],
-      subjects[17]+2,
+      subjects[17],
     ],
     grade10_science: [
       subjects[0],
@@ -78,16 +79,14 @@ export default function Department() {
   };
   return (
     <div className="flex flex-col gap-0">
-      <div className="text-center text-4xl font-semibold my-2 mb-7">
-        <h1>Departments</h1>
-      </div>
-      <div className="self-center flex flex-col gap-4 md:flex-row justify-evenly flex-wrap mb-4">
+      <Header />
+      <div className="self-center relative md:top-[70px] top-[80px] flex flex-col gap-4 md:flex-row justify-evenly flex-wrap mb-4">
         {/*Grade 8 */}
         <Card>
           <h2 className="text-center dark:text-white text-black font-semibold ">GRADE 8:</h2>
           <ul className="">
-            {grade["Grade8"].map((subject) => (
-              <li key={subject}>{subject}</li>
+            {grade["Grade8"].map((subject, index) => (
+              <li key={index}>{subject}</li>
             ))}
           </ul>
         </Card>
--- src/pages/Stuff.tsx
+++ src/pages/Stuff.tsx
@@ -3,183 +3,79 @@ import logo from "../assets/image2.jpg";
 import { Link } from "react-router-dom";
 import { Card } from "flowbite-react";
 import { motion } from "framer-motion";
+import Header from "../components/Header";
 
 export default function Stuff() {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState<string>("");
+
+  const staff = [
+    {
+      id: 1,
+      Subject: "Home Language (IsiZulu)",
+      description:
+        "Here are the teachers who are teaching the home language in Sompukwane Secondary School in reverse chronological order.",
+      link: "/view?subject=Home Language (IsiZulu)",
+    },
+    {
+      id: 2,
+      Subject: "Mathematics",
+      description:
+        "Here are the teachers who are teaching Mathematics in Sompukwane Secondary School in reverse chronological order.",
+      link: "/view?subject=Mathematics",
+    },
+    {
+      id: 3,
+      Subject: "English",
+      description:
+        "Here are the teachers who are teaching English in Sompukwane Secondary School in reverse chronological order.",
+      link: "/view?subject=English",
+    },
+    {
+      id: 4,
+      Subject: "Life Orientation",
+      description:
+        "Here are the teachers who are teaching Life Orientation in Sompukwane Secondary School in reverse chronological order.",
+      link: "/view?subject=Life Orientation",
+    },
+    {
+      id: 5,
+      Subject: "Physical Sciences",
+      description:
+        "Here are the teachers who are teaching Physical Sciences in Sompukwane Secondary School in reverse chronological order.",
+      link: "/view?subject=Physical Sciences",
+    },
+    {
+      id: 6,
+      Subject: "Life Sciences",
+      description:
+        "Here are the teachers who are teaching Life Sciences in Sompukwane Secondary School in reverse chronological order.",
+      link: "/view?subject=Life Sciences",
+    },
+  ]
   return (
     <div className="flex flex-col justify-evenly flex-wrap">
-      <div className="text-center text-4xl font-semibold my-2">
-        <h1 className="uppercase mb-2">Staff</h1>
-      </div>
+      <Header />
 
-      <div className="flex flex-wrap gap-4 justify-evenly my-2">
+      <div className="flex relative top-[80px] md:top-[70px] flex-wrap gap-4 mx-[10px] justify-evenly my-2">
 
         {/*General Teacher*/}
-        <Card className={`max-w-sm 0verflow-hidden transition-transform duration-700 transform ${isHovered ? "scale-105" : "100"} `} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} imgSrc={logo} horizontal>
-          <h5 className={`text-2xl font-bold tracking-tight text-gray-900 dark:text-white ${isHovered ? "-mt-5 duration-700 ease-in-out": "mt-0"}`}>
-            Home Language (IsiZulu)
+        {
+          staff.map(({ id, Subject, description, link }) => (
+            <Card className={`max-w-sm 0verflow-hidden transition-transform duration-700 transform  `} onMouseEnter={() => setIsHovered(Subject)} onMouseLeave={() => setIsHovered("")} imgSrc={logo} horizontal key={id} >
+          <h5 className={`text-[20px] font-semibold font-popins tracking-tight text-gray-900 dark:text-white ${isHovered === Subject ? "-mt-5 duration-700 ease-in-out": "mt-0"}`}>
+            {Subject}
           </h5>
-          <motion.p className="font-normal text-gray-700 dark:text-gray-400" {...(isHovered ? { initial: { opacity: 0 }, animate: { opacity: 1 }, transition: { duration: 0.5 } } : { initial: { opacity: 1 }, animate: { opacity: 0 }, transition: { duration: 0.5 } })}>
-            Here are the teachers who are teaching the home language in
-            Sompukwane Secondary School in reverse chronological order.{" "}
+          <motion.p className="font-robot text-[16px] text-gray-700 dark:text-gray-400" {...(isHovered === Subject ? { initial: { opacity: 0 }, animate: { opacity: 1 }, transition: { duration: 0.5 } } : { initial: { opacity: 1 }, animate: { opacity: 0 }, transition: { duration: 0.5 } })}>
+            {description}
           </motion.p>
-          <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded opacity-0 ease-in-out transition-opacity duration-700 ${isHovered ? "opacity-100 translate-y-4": "translate-y-0"}`}>
-            <Link to="/view" className="">
+          <button className={`bg-blue-500 hover:bg-blue-700 font-robot text-white font-bold py-2 px-4 rounded opacity-0 ease-in-out transition-opacity duration-700 ${isHovered === Subject ? "opacity-100 translate-y-4": "translate-y-0"}`}>
+            <Link to={link} className="">
               view
             </Link>
           </button>
-        </Card>  
-
-        {/*General Teacher*/}
-        <Card className="max-w-sm hover:scale-105" imgSrc={logo} horizontal onMouseOver={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            First Additional Language (English)
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers who are teaching the first additional language
-            in Sompukwane Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        {/*/maths Teachers */}
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Mathematics
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers who are teaching the Mathematics in Sompukwane
-            Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        {/*History Teachers*/}
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Mathematics (Literacy)
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers that are teaching the home language in
-            Sompukwane Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        {/*Geography */}
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Physical Sciences
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers who are teaching the physical sciences in
-            Sompukwane Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        {/*Drama */}
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Drama
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers who are teaching the drama in Sompukwane
-            Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        {/*Tourism Teachers */}
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Tourism
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers that are teaching the tourism in Sompukwane
-            Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        {/*Accounting Teachers*/}
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Accounting
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers that are teaching the home language in
-            Sompukwane Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        {/* */}
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Business Studies
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers that are teaching the home language in
-            Sompukwane Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Life Sciences
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers that are teaching the home language in
-            Sompukwane Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Life Sciences
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers who are teaching the life orientation in
-            Sompukwane Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
-
-        <Card className="max-w-sm" imgSrc={logo} horizontal>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Consumer
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the teachers who are teaching the consumer in Sompukwane
-            Secondary School in reverse chronological order.{" "}
-            <Link to="/view" className="text-blue-700">
-              view
-            </Link>
-          </p>
-        </Card>
+        </Card> 
+          ))
+        } 
       </div>
     </div>
   );
