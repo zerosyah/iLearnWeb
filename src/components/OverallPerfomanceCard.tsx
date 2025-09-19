1eef9d12c449ca515232ff43a712bfdcb31b27fb
Siyabonga Shezi <154099964+zerosyah@users.noreply.github.com>
2025-09-19 05:25:02 +0200

    Update OverallPerfomanceCard.tsx update2

--- src/components/OverallPerfomanceCard.tsx
+++ src/components/OverallPerfomanceCard.tsx
@@ -1,8 +1,8 @@
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import { SelectChangeEvent } from "@mui/material/Select";
-import React from "react";
+//import FormControl from "@mui/material/FormControl";
+//import { SelectChangeEvent } from "@mui/material/Select";
+//import React from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
 
 function OverallPerfomanceCard() {
