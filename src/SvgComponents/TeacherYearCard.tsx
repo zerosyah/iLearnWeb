fac80e58bbe62c12831c5e8a016f12bdeb779f36
zerosyah <syahshezi3000@gmail.com>
2026-06-01 18:23:04 +0200

    maque  overflow fix

--- src/SvgComponents/TeacherYearCard.tsx
+++ src/SvgComponents/TeacherYearCard.tsx
@@ -22,17 +22,17 @@ const containerStackStyle = {
 function TeacherYearCard({title, name, subject, grade, description, img}:TeacherYearCardProps):JSX.Element {
   return (
     <Box sx={[containerBoxStyle, {marginRight:2, marginLeft:2}]}>
-      <Stack sx={containerStackStyle} className='bg-gradient-to-tr from-defaultYellow to-defaultGreen shadow-md shadow-black'>
+      <Stack sx={containerStackStyle} className='bg-gradient-to-tr from-defaultYellow to-defaultGreen shadow-md shadow-black overflow-hidden'>
         <h1 className="text-[30px] font-bebasNeue text-center font-bold leading-0 ">{title}</h1>
         <Stack className='absolute bottom-0' direction={"row"} gap={1}>
             <div className="w-[140px] h-[150px] border-black rounded-[5px]">
                 <img src={teacherImages[img]} alt="text image" className='w-[140px] h-[150px] object-fill rounded-[6px]' />
             </div> 
             <Stack className='w-[210px] h-[150px] left-0 rounded-[7px] border-blue-400'>
-                <h2 className="text-[18px] font-pacifico font-bold uppercase">{name}</h2>
-                <h3 className='text-[16px] leading-0 font-montserrat font-semibold'>{subject}</h3>
-                <h4 className="text-[14px] font-montserrat font-semibold">{grade}</h4>
-                <p className="text-[12px] pt-[12px] font-semibold font-nunito text-wrap">{description}</p>
+                <h2 className="text-[16px] font-pacifico font-bold uppercase">{name}</h2>
+                <h3 className='text-[14px] leading-0 font-montserrat font-semibold'>{subject}</h3>
+                <h4 className="text-[12px] font-montserrat font-semibold">{grade}</h4>
+                <p className="text-[10px] pt-[1px] font-semibold font-nunito text-wrap">{description}</p>
             </Stack>
         </Stack>
       </Stack>
